7aa1fe5c1553deca72fb0c7b8db144e125baca02
konojunya <junya03dance@gmail.com>
2018-01-16 10:42:26 +0900

    サンプルを写経して試す

--- app.js
+++ app.js
@@ -18,4 +18,29 @@ Rx.Observable.range(0, 10)
   .subscribe((msg) => console.log(msg))
 */
 
+// distinctUntilChangedオペレータを使う
+/*
+const source = Rx.Observable.of(0, 0, 1, 2, 1, 2, 2)
+source.subscribe((n) => process.stdout.write(`${n} `))
+console.log("\n")
 
+source.distinctUntilChanged()
+.subscribe((n) => process.stdout.write(`${n} `))
+*/
+
+// ストリームで時間軸を取り扱う
+// intervalのソースは10msですが、2000ms(2秒)経つまでのメッセージを破棄するのがthrottleTime
+/*
+Rx.Observable.interval(10)
+.throttleTime(2000)
+.subscribe((n) => console.log(n, new Date().toLocaleDateString()))
+*/
+
+// エラーハンドリング
+/*
+Rx.Observable.throw(new Error('hoge'))
+.subscribe(
+  () => console.log("waai"),
+  (err) => console.log(`handling!: ${err.toString()}`)
+)
+*/
